c399c5072348882c539445cf8b4bf15388479cd7
UJJWAL SHARMA <92508362+ujjwal0-2@users.noreply.github.com>
2024-09-01 18:11:46 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,15 @@
+# Data / Values  can be stored in temporary storage called Variables 
+
+student="Sam"
+print(student)
+
+student ="Atul"
+print(student)
+
+#Every variable  is associated with data-type
+# int, floating , boolean , string
+
+a1=10
+type(a1)
+
+# keywords are special reserved words
