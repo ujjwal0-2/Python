a3a4175bd65899527e82c972091bb576ded9ed2c
UJJWAL SHARMA <92508362+ujjwal0-2@users.noreply.github.com>
2024-09-02 15:43:09 +0530

    Add files via upload

---
+++ identifier.py
@@ -0,0 +1,30 @@
+# Identifers are the names used for varibles,functions or objects
+
+#Rules
+#No special charcater expect underscore
+#identifers are case-sensitive
+#first letter cannot be Digit
+student="martha"
+student="mia"
+print(student)
+
+#Literals which cannot change
+a=3.14
+
+#string- sequence of character
+#'hello'
+#"python"
+#`"Mia"`
+
+my_string="My name is john"
+my_string[-1]
+
+
+#FInding length of string -   len(my_string)
+#Converting to lower case -  my_string.lower()   upperscase my_string.upper()
+
+# Replacing a substring -  my_string.replace('a','y')
+#Number of occurrence -  new_string.count("hello")
+
+#Finding index of substring- s1.find()
+# Splitting - fruit.split()
