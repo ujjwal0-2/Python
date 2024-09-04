29bb8d09593856fa364a818100abfd659992e134
UJJWAL SHARMA <92508362+ujjwal0-2@users.noreply.github.com>
2024-09-04 14:39:58 +0530

    Add files via upload

---
+++ Statement.py
@@ -0,0 +1,26 @@
+#a=10
+#b=20
+#if b>a: if and else statement
+ #   print("B is greater than A")
+  
+  
+  #if with tuple
+#i=1
+#n=2
+#while i<=10: while loop
+    #print(n," * ",i," = ",n*i)
+   # i=i+1
+    
+#l1=['mango','apple','chair']
+#for i in l1:
+ #   print(i)
+# for loop
+l1=['mango','apple','chair']
+l2=['man','aim','creep']  
+for i in l1:
+    for j in l2:
+        print(i,j)
+
+
+
+    
